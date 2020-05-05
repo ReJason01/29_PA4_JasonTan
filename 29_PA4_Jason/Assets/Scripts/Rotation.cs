6997675b983ddd142e92f764bcd329e4bad61ff5
ReJason01 <64124407+ReJason01@users.noreply.github.com>
2020-05-05 11:20:51 +0800

    Unity Project Added
    I just added 1 new Unity Project!

---
+++ 29_PA4_Jason/Assets/Scripts/Rotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    [SerializeField] float spd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.right * spd * Time.deltaTime);
+    }
+}
+
+
