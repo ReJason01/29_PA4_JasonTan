abee2f7bccba42df1b51947345e3a14f306f4579
DebuDebuKing <48768830+DebuDebuKing@users.noreply.github.com>
2020-05-05 12:38:51 +0800

    Update Rotation.cs
    Commited to master

---
+++ 29_PA4_Jason/Assets/Scripts/Rotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    [SerializeField] float spd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(spd ==0)
+        {
+            spd = 300;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.right * spd * Time.deltaTime);
+    }
+}
+
+
